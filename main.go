e9905c706627c3f28f6d9f1c0671898228df6e66
suryaa6666 <suryaelidanto@gmail.com>
2023-02-20 11:52:57 +0700

    result

--- main.go
+++ main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
+	// Create new Echo instance
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World!")
-	})
+	t := &Template{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
 
-	e.GET("/about", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello About")
-	})
+	e.Renderer = t
 
+	// Routing
+	e.GET("/hello", helloWorld)
+	e.GET("/", home)
+
+	// Start server
+	println("Server running on port 5000")
 	e.Logger.Fatal(e.Start("localhost:5000"))
+}
+
+func helloWorld(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World")
+}
+
+func home(c echo.Context) error {
+	return c.Render(http.StatusOK, "index.html", nil)
 }
